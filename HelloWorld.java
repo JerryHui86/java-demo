141750e1091f64d17ea6e7b23dbf483de74de24d
JerryHui86 <jerryhui416@gmail.com>
2025-09-01 15:16:41 -0700

    a

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,8 @@
 public class HelloWorld {
     public static void main(String[] args) {
+        int x = 10;
+        int y = 20;
+        x += 5;
         System.out.println("Hello, World!");
     }
 }
